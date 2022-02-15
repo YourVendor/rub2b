3e53dcaf60c7e8768de03d6cd9889188ff92ab1e
Vladimir <yourvendor@ya.ru>
2022-02-15 20:24:34 +0300

    Docker Postgres Gunicorn Nginx ProductionDocker + static & media

--- rub2b_dev/rub2b_server/settings.py
+++ rub2b_dev/rub2b_server/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'upload',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
